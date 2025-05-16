4396d81838c0f4169e3d78a141b17c5e3c5a1767
thuyy083 <142137343+thuyy083@users.noreply.github.com>
2025-05-16 08:52:08 +0700

    list user UI

--- src/routers/admin.route.js
+++ src/routers/admin.route.js
@@ -2,6 +2,8 @@ import { Routes } from '~/config'
 import AdminLayout from '~/layouts/AdminLayout'
 import Dashboard from '~/pages/Admin/Dashboard'
 import UserCreate from '~/pages/Admin/User/Create'
+import UserList from '~/pages/Admin/User/list'
+
 
 export default [
   {
@@ -13,5 +15,10 @@ export default [
     path: Routes.admin.user.create,
     layout: AdminLayout,
     component: UserCreate
+  },
+  {
+    path: Routes.admin.user.list,
+    layout: AdminLayout,
+    component: UserList,
   }
 ]
