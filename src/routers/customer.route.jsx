70c1b5563d7f8a73e65f1052814ee8594f979819
kimngan05032002 <trankimngan05032002@gmail.com>
2025-06-20 16:28:35 +0700

    chỉnh sửa profile customer

--- src/routers/customer.route.jsx
+++ src/routers/customer.route.jsx
@@ -1,16 +1,14 @@
 import DetailItem from '~/pages/Customer/DetailItem'
 import CustomerLayout from '~/layouts/CustomerLayout'
 import HomePage from '~/pages/Customer/Home'
-<<<<<<< HEAD
 import ListItem from '~/pages/Customer/ListItem'
-=======
 import ProfileCustomer from '~/pages/Customer/Profile'
->>>>>>> 18a3bdb2b83b0422b51d7e94bd7d5126e320bf49
+import EditProfile from '~/pages/Customer/Profile/edit'
 export default [
   {
     path: '/customer',
     element: <CustomerLayout />,
-    children : [
+    children: [
       {
         // path: 'detail-Item',
         path: 'detail-Item/:id',
@@ -22,13 +20,16 @@ export default [
         element: <HomePage />
       },
       {
-<<<<<<< HEAD
         path: 'list-Item',
         element: <ListItem />
-=======
+      },
+      {
         path: 'profileCustomer',
         element: <ProfileCustomer />
->>>>>>> 18a3bdb2b83b0422b51d7e94bd7d5126e320bf49
+      },
+      {
+        path: 'editProfile',
+        element: <EditProfile />
       }
     ]
   },
